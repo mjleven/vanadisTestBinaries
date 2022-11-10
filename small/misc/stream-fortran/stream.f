bb5ef2da02accc4405a96da0f264ca7c8cb8109f
mjleven <mjleven@sandia.gov>
2022-11-10 15:11:59 -0700

    add stream test

---
+++ small/misc/stream-fortran/stream.f
@@ -0,0 +1,12 @@
+      PROGRAM stream
+      INTEGER n
+      n= 256 
+      WRITE (*,*) n
+      n= 512 
+      WRITE (*,*) 'Array size = ',n
+      n= 768 
+      WRITE (*,*) 'Array size = ',n
+      n= 1024 
+      WRITE (*,*) 'Array size = ',n
+
+      END
